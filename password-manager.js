b36619299b68b79cef40915d42a376e9bed08c85
ryandiaz <diazry@gmail.com>
2015-02-12 01:03:50 -0500

    Not running code so there's sure to be syntax probs
    * conceptual questions commented in

--- password-manager.js
+++ password-manager.js
@@ -52,26 +52,32 @@ var keychain = function() {
   
   //R START RYAN ADDED VARS
   //R salt public for now. does the salt need to be private or change?
-  var salt = 1;
-  var key = null;
+  var salt = null;
+  var key_gcm = null; //R for encryption decryption
+  var key_HMAC = null; //R for HMAC signatures
   //R END
 
-  var keychain = {};
+  var keychain = {}; //R SO This is the apparent store for the key-values 
 
   /** 
     * Creates an empty keychain with the given password. Once init is called,
     * the password manager should be in a ready state.
-    *
+    * O(1)
     * Arguments:
     *   password: string
     * Return Type: void
     */
   keychain.init = function(password) {
     //R check the master password? or use key produce to decrypt other passswords?
-    key = KDF(password, salt);
+    var init_salt = random_bitarray(256);
+    var init_key = KDF(password, init_salt);//R how can we generate separate encryption and HMAC keys with one KDF call?
+    //R for now they are the same but I think this should change
+    key_gcm = init_key;
+    key_HMAC = init_key;
+    salt = init_salt;
     ready = true;
     priv.data.version = "CS 255 Password Manager v1.0";
-    priv.data.checksum = 0;
+    priv.data.checksum = null;
     
   };
 
@@ -85,7 +91,7 @@ var keychain = function() {
     * the representation passed to load is well-formed (e.g., the result of a 
     * call to the save function). Returns true if the data is successfully loaded
     * and the provided password is correct. Returns false otherwise.
-    *
+    * O(n)
     * Arguments:
     *   password:           string
     *   repr:               string
@@ -94,7 +100,14 @@ var keychain = function() {
     */
   keychain.load = function(password, repr, trusted_data_check) {
     //R the secrets will contain the encrypted key-value store 
-    throw "Not implemented!";
+    var load_key = KDF(password, salt);
+    //R take load_key to verify the password is correct
+    //R check if the password is correct if not return false
+    if (!bitarray_equal(trusted_data_check, SHA256(string_to_bitarray(repr)))) throw "integrity check fails!";
+    //R trusted_data_check should equal the checksum value in the repsentation, if not throw exception
+    keychain = JSON.parse(repr);
+    
+    return true;
 
   };
 
@@ -108,11 +121,17 @@ var keychain = function() {
     * all of the data in the password manager. The second element is a SHA-256
     * checksum computed over the password manager to preserve integrity. If the
     * password manager is not in a ready-state, return null.
-    *
+    * O(n)
     * Return Type: array
     */ 
   keychain.dump = function() {
-    throw "Not implemented!";
+    //R tentatively implemented
+    if (!ready) return null
+    var keychain_str = JSON.stringify(keychain)
+    var checksum = SHA256(string_to_bitarray(keychain_str)) //R SHA256 takes in bit array
+   
+    //R throw "Not implemented";
+    return [keychain_str, checksum]
   }
 
   /**
@@ -120,13 +139,21 @@ var keychain = function() {
     * If there is no entry in the KVS that matches the given domain, then return
     * null. If the password manager is not in a ready state, throw an exception. If
     * tampering has been detected with the records, throw an exception.
-    *
+    * O(1)
     * Arguments:
     *   name: string
     * Return Type: string
     */
   keychain.get = function(name) {
-    throw "Not implemented!";
+    var result, pad_len, pass_padded;
+    if (!ready) throw "Keychain not initialized.";
+    //R how to check checksum?
+    //R could strignify keychain, run HMAC and check if equal to value stored in priv
+    result = keychain[HMAC(key_HMAC, name)];
+    if (result === undefined) return null;
+    pass_padded = dec_gcm(setup_cipher(key_gcm),result);
+    pad_len = pass_padded.slice(-1); //R should we need to check every pad byte? look at lecture to double check
+    return pass_padded.substring(0,MAX_PW_LEN_BYTES - pad_len);
   }
 
   /** 
@@ -134,21 +161,30 @@ var keychain = function() {
   * already in the password manager, this method should update its value. If
   * not, create a new entry in the password manager. If the password manager is
   * not in a ready state, throw an exception.
-  *
+  * O(1)
   * Arguments:
   *   name: string
   *   value: string
   * Return Type: void
   */
   keychain.set = function(name, value) {
-    throw "Not implemented!";
+    //R signature for name and encrypted value are put in keychain
+    var pad_lenth = MAX_PW_LEN_BYTES - value.length + 1;
+    var chr_pad = String.fromCharCode(pad_length);
+    if (!ready) throw "Keychain not initialized.";
+    if (pad_length < 1) throw "Password max length exceeded";
+    value = value + Array(pad_length+1).join(chr_pad) //R pad makes value 65 bytes long, at decrypt look at char val and remove that many from end
+    var domain_sig = HMAC(key_HMAC, name);
+    var pass_enc = enc_gcm(setup_cipher(key_gcm), value);
+    keychain[domain_sig] = pass_enc;
+ 
   }
 
   /**
     * Removes the record with name from the password manager. Returns true
     * if the record with the specified name is removed, false otherwise. If
     * the password manager is not in a ready state, throws an exception.
-    *
+    * O(1)
     * Arguments:
     *   name: string
     * Return Type: boolean
